0f48386261eff6d8d10b063808a61e47b1c400a7
gahyun <gahyun727301@kangnam.ac.kr>
2024-01-15 20:24:34 +0900

    Hide subcategories in Category component

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -10,22 +10,22 @@ const Category = () => {
       <ul className="flex relative items-center">
         <li className="pt-10 relative float-left text-sm">
           <a className=" gr p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline} rounded-full hover:{bg-green-900 text-white underline}">아우터</a>
-          <ul className="sub">
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">가디건/조끼</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">야상/점퍼</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">자켓/코트</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">패딩</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">플리스</a></li>
+          <ul className="hidden">
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">가디건/조끼</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">야상/점퍼</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">자켓/코트</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">패딩</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">플리스</a></li>
           </ul>
         </li>
         <li className="top">
           <a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">상의</a>
-          <ul className="sub">
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">긴팔티셔츠</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">맨투맨</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">후드</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">반팔/민소매티셔츠</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">니트</a></li>
+          <ul className="hidden">
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">긴팔티셔츠</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">맨투맨</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">후드</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">반팔/민소매티셔츠</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">니트</a></li>
           </ul>
         </li>
         <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">셔츠/블라우스</a></li>
@@ -35,26 +35,26 @@ const Category = () => {
         <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">커트</a></li>
         <li className="top">
           <a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">팬츠</a>
-          <ul className="sub">
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">청바지</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">면바지</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">슬랙스</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">와이드팬츠</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">숏팬츠</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">조거팬츠</a></li>
-            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">기장별</a></li>
+          <ul className="hidden">
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">청바지</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">면바지</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">슬랙스</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">와이드팬츠</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">숏팬츠</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">조거팬츠</a></li>
+            <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline g-opacity-80}">기장별</a></li>
           </ul>
         </li >
         <li className="top">
           <a>가방</a>
-          <ul className="sub">
+          <ul className="hidden">
             <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">백팩/스쿨백</a></li>
             <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">크로스/토트백</a></li>
           </ul>
         </li >
         <li className="top">
           <a>신발</a>
-          <ul className="sub">
+          <ul className="hidden">
             <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">운동화/단화</a></li>
             <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">구두/워커</a></li>
             <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">샌들/슬리퍼/장화</a></li>
@@ -62,7 +62,7 @@ const Category = () => {
         </li >
         <li className="top">
           <a>악세서리</a>
-          <ul className="sub">
+          <ul className="hidden">
             <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">주얼리</a></li>
             <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">패션소품</a></li>
             <li><a className="p-2 no-underline text-black bg-orange-200 font-bold rounded-full hover:{bg-green-900 text-white underline}">양말/스타킹</a></li>
